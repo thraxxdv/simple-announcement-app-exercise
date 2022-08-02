0d4a21a9c970e04edf4bcbcdda8d1227a099daa6
Dylan Villaruel <thraxxdv@gmail.com>
2022-08-02 10:23:31 +0800

    build: added constant test user in db seeder for easier login and testing

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,6 +17,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         \App\Models\User::factory(10)->create();
+        User::create([
+            'name' => 'Test User',
+            'email' => 'test@test.com',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi'
+        ]);
         $this->call([
             AnnouncementSeeder::class
         ]);
