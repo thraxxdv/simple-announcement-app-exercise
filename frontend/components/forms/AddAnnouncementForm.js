4548693de14aa4e647569d66cdfe264f43ee29eb
Dylan Villaruel <thraxxdv@gmail.com>
2022-08-02 22:36:52 +0800

    build: UI scaffolding for adding new announcement

--- frontend/components/forms/AddAnnouncementForm.js
+++ frontend/components/forms/AddAnnouncementForm.js
@@ -1,9 +1,55 @@
-import React from 'react'
+import React, { useState } from "react";
+import Input from "./elements/Input";
+import Button from "./elements/Button";
 
 function AddAnnouncementForm() {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+
   return (
-    <div>AddAnnouncementForm</div>
-  )
+    <form action="" className="p-5 shadow mt-4 rounded">
+      <h1 className="fw-bold mb-4">Add New Announcement</h1>
+      <Input
+        name="title"
+        containerClass="mb-4"
+        label="Title:"
+        value={title}
+        onChangeCb={(e) => setTitle(e.target.value)}
+        placeholder="Title"
+      />
+      <div className="mb-4">
+        <label htmlFor="content">Content:</label>
+        <textarea
+          className="form-control"
+          id="content"
+          name="content"
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+        ></textarea>
+      </div>
+      <div className="d-flex">
+        <Input
+          name="start_date"
+          label="Start Date:"
+          type="date"
+          value={startDate}
+          onChangeCb={(e) => setStartDate(e.target.value)}
+          containerClass="flex-grow-1 me-2"
+        />
+        <Input
+          name="end_date"
+          label="End Date:"
+          type="date"
+          value={endDate}
+          onChangeCb={(e) => setEndDate(e.target.value)}
+          containerClass="flex-grow-1 ms-2"
+        />
+      </div>
+        <Button label="Add Announcement" additionalClasses="mt-4 mx-auto d-block" />
+    </form>
+  );
 }
 
-export default AddAnnouncementForm
+export default AddAnnouncementForm;
--- frontend/pages/dashboard.js
+++ frontend/pages/dashboard.js
@@ -1,10 +1,20 @@
 import React from "react";
 import AuthenticatedLayout from "../components/layouts/AuthenticatedLayout";
+import AddAnnouncementForm from "../components/forms/AddAnnouncementForm";
 
 function Dashboard() {
   return (
     <AuthenticatedLayout>
-      <div>dashboard</div>
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-5">
+            <AddAnnouncementForm />
+          </div>
+          <div className="col-lg-7">
+            
+          </div>
+        </div>
+      </div>
     </AuthenticatedLayout>
   );
 }
